aea0cb04649a744b37c12b94144ac86b1f2a9bf4
ilyas-homedev <ilyasov000fp@gmail.com>
2021-07-11 22:51:26 +0200

    base structure

---
+++ plugins/modal.js
@@ -0,0 +1,60 @@
+function _createModal(options) {
+    const modal = document.createElement('div');
+    modal.classList = 'vmodal';
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal-overlay">
+            <div class="modal-window">
+                <div class="modal-header">
+                    <span class="modal-title">Modal title</span>
+                    <span class="modal-close">&times;</span>
+                </div>
+                <div class="modal-body">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit.</p>
+                </div>
+                <div class="modal-footer">
+                    <button>Ok</button>
+                    <button>Cancel</button>
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.appendChild(modal);
+    return modal;
+}
+
+/*
+* Title: string
+* Closable: boolean
+* Content: string
+* Width: string ('400px')
+* Destroy(): void
+* Close modal window with cross or clicking on overlay
+* setContent(html: string): void | public method
+* onClose(): void
+* onOpen(): 
+* beforeClose()
+*-----------------------
+* animate.css
+*/
+$.modal = function(options) {
+    const ANIMATION_SPEED = 200;
+    const $modal = _createModal(options);
+    let closing = false;
+
+    return {
+        open() {
+            !closing && $modal.classList.add('open');
+        },
+        close() {
+            closing = true;
+            $modal.classList.remove('open');
+            $modal.classList.add('hide');
+            setTimeout(() => {
+                $modal.classList.remove('hide');
+                closing = false;
+            }, ANIMATION_SPEED);
+        },
+        destroy() {}
+    }
+}
